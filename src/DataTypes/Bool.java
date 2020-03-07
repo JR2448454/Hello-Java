6a4ae558b56fdffe4926bfb65729594b2711d54b
JAY RATHOD <jayantirathod56@gmail.com>
2020-03-07 13:43:02 +0530

    first commit

---
+++ src/DataTypes/Bool.java
@@ -0,0 +1,13 @@
+package DataTypes;
+
+public class Bool {
+    public static void main(String[] args){
+        boolean a = false;
+        if (a==true){
+            System.out.print("Yes...!");
+        }
+        else {
+            System.out.print("Noooo...!");
+        }
+    }
+}
